09c02309fec6aee87b3e7d6bbaf37bf670f80ece
Administrator <Administrator@192.168.0.84>
2020-02-21 17:39:03 +0900

    sdsd

---
+++ src/com/human/ex/helloworld4.java
@@ -0,0 +1,19 @@
+/*
+ 이 프로그램은 초보자들을 위한 처음시작하는
+ 프로그램 코드입니다.
+ 20200219
+ */
+
+package com.human.ex; //폴더 경로
+
+public class helloworld4 // 클래스 이름
+{
+
+	public static void main(String[] args) //main은 프로그램 시작부분
+	{
+				System.out.println("안녕자바"); //문제 1
+				
+
+	}
+
+}
